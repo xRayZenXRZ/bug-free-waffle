b1cf2fe05333e132d6846a09715dac0864799c41
Erdene <tsogt-erdene.altantuya@etu.univ-lorraine.fr>
2026-04-20 18:50:11 +0200

    + Verification : domaine/Prestation.py

--- src/domaine/Prestation.py
+++ src/domaine/Prestation.py
@@ -4,7 +4,31 @@ class Prestation:
 
     leDAOPrestation = DAOPrestation.get_instance()
 
-    def __init__(self, id_prestation=None, date_prevue=None, date_effective=None, lieu=None, type_prestation=None, nb_photos_prevues=None, nb_videos_prevues=None, numero_contrat=None):
+    def __init__(self, id_prestation : int = None, date_prevue : str =None, date_effective : str = None, lieu : str =None, type_prestation : str =None, nb_photos_prevues : int =None, nb_videos_prevues : int =None, numero_contrat : str =None):
+        
+        #verification type : 
+        if id_prestation is not None and not isinstance(id_prestation, int):
+            raise TypeError("l'attribut {id_prestation} doit être un entier")
+        if date_prevue is not None and not isinstance(date_prevue, str):
+            raise TypeError("l'attribut {date_prevue} doit être une chaîne de caractères")
+        if date_effective is not None and not isinstance(date_effective, str):
+            raise TypeError("l'attribut {date_effective} doit être une chaîne de caractères")
+        if lieu is not None and not isinstance(lieu, str):
+            raise TypeError("l'attribut {lieu} doit être une chaîne de caractères")
+        if type_prestation is not None and not isinstance(type_prestation, str):
+            raise TypeError("l'attribut {type_prestation} doit être une chaîne de caractères")
+        if nb_photos_prevues is not None and not isinstance(nb_photos_prevues, int):
+            raise TypeError("l'attribut {nb_photos_prevues} doit être un entier")
+        if nb_videos_prevues is not None and not isinstance(nb_videos_prevues, int):
+            raise TypeError("l'attribut {nb_videos_prevues} doit être un entier")
+        if numero_contrat is not None and not isinstance(numero_contrat, str):
+            raise TypeError("l'attribut {numero_contrat} doit être une chaîne de caractères")
+
+        # Vérification des valeurs numériques positives -> à refaire sur les autres classes...
+        if nb_photos_prevues is not None and nb_photos_prevues < 0:
+            raise ValueError("le nombre de photos prévues ne peut pas être négatif")
+        if nb_videos_prevues is not None and nb_videos_prevues < 0:
+            raise ValueError("le nombre de vidéos prévues ne peut pas être négatif")
 
         self.__date_prevue = date_prevue
         self.__date_effective = date_effective
