8e0ac04c89386e94a61fdcc5fdad92f04cf05370
Erdene <tsogt-erdene.altantuya@etu.univ-lorraine.fr>
2026-05-02 18:16:35 +0200

    suppression de commentaire inutile

--- src/domaine/Paiement.py
+++ src/domaine/Paiement.py
@@ -80,37 +80,3 @@ class Paiement:
 
     def __str__(self):
         return f"Paiement(id_paiement={self.__id_paiement}, date={self.__date}, montant={self.__montant}, numeroFacture={self.__numero_Facture})"
-
-
-"""class Paiement :
-
-    def __int__(self, id_paiement, montant, enum_paiements_conditions ):
-        self.__id_paiement = id_paiement
-        self.__montant = montant
-        self.__enum_paiements_conditions = enum_paiements_conditions
-
-    # Getters
-    def get_id_paiement(self):
-        return self.__id_paiement
-
-    def get_montant(self):
-        return self.__montant
-
-    def get_enum_paiements_conditions(self):
-        return self.__enum_paiements_conditions
-
-    # Setters
-
-    #commentaire : toujours la même chose à faire ...
-    
-    def set_id_paiement(self, id_paiement):
-        self.__id_paiement = id_paiement
-
-    def set_montant(self, montant):
-        self.__montant = montant
-
-    def set_enum_paiements_conditions(self, enum_paiements_conditions):
-        self.__enum_paiements_conditions = enum_paiements_conditions
-
-    def __str__(self):
-        return f"Paiement(id_paiement={self.__id_paiement}, montant={self.__montant}, conditions_paiements={self.__enum_paiements_conditions})"""
