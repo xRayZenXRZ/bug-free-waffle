c315e7d6e7c44476edd59ff718faa1f235132ae3
xRayZenXRZ <tsogt-erdene.altantuya2@etu.univ-lorraine.fr>
2026-05-05 13:19:25 +0200

    add Gestion Prestation Activite

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from Interface_Tkinter.gestion_client import GestionClient
+from Interface_Tkinter.gestion_prestations_devis import GestionPrestationActivite
 from Interface_Tkinter.main_windows import MainWindow
 from Interface_Tkinter.Acceuil import ConnexionUI
 import tkinter as tk
@@ -34,7 +35,7 @@ class App:
 
         # Afficher main window
         MainWindow(self.container, utilisateur,
-                   self.afficher_gestion_utilisateurs,self.afficher_gestion_clients,self.afficher_gestion_devis)
+                   self.afficher_gestion_utilisateurs,self.afficher_gestion_clients,self.afficher_gestion_devis, self.afficher_gestion_prestation_activite)
 
     def afficher_gestion_utilisateurs(self):
         # Nettoyer le container
@@ -59,6 +60,14 @@ class App:
 
         # Afficher la page de gestion
         GestionDevis(self.container, self.utilisateur_connecte,on_back=lambda: self.afficher_main_window(self.utilisateur_connecte))
+
+    def afficher_gestion_prestation_activite(self):
+        # Nettoyer le container
+        for widget in self.container.winfo_children():
+            widget.destroy()
+
+        # Afficher la page de gestion
+        GestionPrestationActivite(self.container, self.utilisateur_connecte,on_back=lambda: self.afficher_main_window(self.utilisateur_connecte))
 
 
 if __name__ == "__main__":
